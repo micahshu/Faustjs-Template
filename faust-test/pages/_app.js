bfbfa4d6c1bff2a0af598b1578a9e7865a13ee13
Micah Shumaker <micah.shumaker@gmail.com>
2024-03-20 12:35:00 -0600

    first commit

---
+++ faust-test/pages/_app.js
@@ -0,0 +1,27 @@
+import '../faust.config';
+import React from 'react';
+import { useRouter } from 'next/router';
+import { FaustProvider } from '@faustwp/core';
+import '@faustwp/core/dist/css/toolbar.css';
+import '../styles/global.scss';
+import { WordPressBlocksProvider } from '@faustwp/blocks';
+import blocks from '../wp-blocks';
+
+
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  return (
+    <FaustProvider pageProps={pageProps}>
+      <WordPressBlocksProvider
+        config={{
+          blocks,
+          theme: null,
+        }}
+      >
+        <Component {...pageProps} key={router.asPath} />
+      </WordPressBlocksProvider>
+    </FaustProvider>
+  );
+}
